e81e86b1670900d66df990225e636b7fc66fd389
MohamedSaafan <201827254@std.sci.cu.edu.eg>
2023-01-29 13:16:49 +0200

    added the state abbreviations to the states script

--- src/insert-states-to-db.js
+++ src/insert-states-to-db.js
@@ -1,14 +1,25 @@
 const { states } = require("./constants/states-string");
-
+const { statesAbbreviations } = require("./constants/states_abbreviations");
 // confirm them into an array of object
-
+const abbreviationsKeys = Object.keys(statesAbbreviations);
 const statesStrArr = states.split("\n");
 const statesObjARr = statesStrArr.map((stateStr) => {
   const stateFips = +stateStr.trim().substring(0, 2);
   const stateLong = stateStr.trim().substring(2).trim();
+  let stateAbbr;
+  for (let i = 0; i < abbreviationsKeys.length; i++) {
+    if (
+      statesAbbreviations[abbreviationsKeys[i]].toLowerCase() ===
+      stateLong.toLowerCase()
+    ) {
+      stateAbbr = abbreviationsKeys[i];
+      break;
+    }
+  }
   return {
     stateFips,
     stateLong,
+    stateAbbr,
   };
 });
 console.log(statesObjARr, "from ");
